bee3dd90ad3651b1213ea7214f500cd95ec47fec
AjagareRahul <ajagarerahul@gmail.com>
2026-02-05 16:38:27 +0530

    uploadimg

--- uploadproject/testapp/views.py
+++ uploadproject/testapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from testapp.models import Image
+from testapp.form import UploadForm
+
 
 # Create your views here.
+def upload_img(request):
+    if request.method=="POST":
+        form=UploadForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+            #return render(request,'testapp/ulpoadimg.html',{'form':form})
+        
+    form= UploadForm() 
+    img=Image.objects.all()
+    return render(request,'testapp/ulpoadimg.html',{'img':img,'form':form})
